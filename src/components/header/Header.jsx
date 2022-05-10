373f365e8ed6e435bd0bb07e840165c5fcdc68d7
Enock  Agwera <enock.agwera@Enocks-MacBook-Pro.local>
2022-05-10 20:56:29 +0300

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './header-styles.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faBagShopping, faHeart } from '@fortawesome/free-solid-svg-icons'
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+const Header = () => {
+  return (
+    <div className='container'>
+        <div>
+            <p className='header-title'>OYOTEE</p>
+        </div>
+
+        <div className='nav-menu'>
+            <p>Shop</p>
+            <p>Plant Care</p>
+            <p>Workshops</p>
+            <p>Blogs</p>
+            <FontAwesomeIcon icon="fa-light fa-bag-shopping" />
+        </div>
+
+        <div className='icons-right'>
+            <div className='cart'>
+                <ShoppingBagOutlinedIcon />
+            </div>
+            <FavoriteBorderIcon/>
+            <img src="https://image.shutterstock.com/image-photo/indoor-portrait-beautiful-brunette-young-260nw-640005220.jpg" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/main/main.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import Header from '../header/Header'
+import './main-styles.css'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const main = () => {
+    return (
+        <div>
+            <Header />
+
+            <div className='body'>
+                <div className='sidebar-filter'>
+                    <p className='filter'>Filter</p>
+                    <p className='categories'>Categories</p>
+
+                    <div className='filter-item'>
+                        <input type='checkbox' />
+                        <p>Gardening</p>
+                    </div>
+                    <div className='filter-item'>
+                        <input type='checkbox' />
+                        <p>Plants</p>
+                    </div>
+                    <div className='filter-item'>
+                        <input type='checkbox' />
+                        <p>Seeds</p>
+                    </div>
+                    <div className='filter-item'>
+                        <input type='checkbox' />
+                        <p>Bulbs</p>
+                    </div>
+                    <div className='filter-item'>
+                        <input type='checkbox' />
+                        <p>Planters</p>
+                    </div>
+
+                    <div className='others'>
+                        <p>Others</p>
+                        <ExpandMoreIcon />
+                    </div>
+
+                    <div className='price-range'>
+                        <p className='price-range-title'>Price Range</p>
+                        <div className='top-div'>
+                            <div className='min'>
+                                <p>Min</p>
+                            </div>
+                            <div className='price'>
+                                <p>$400</p>
+                            </div>
+                        </div>
+                        <button className='bottom-button'>
+                            <p>Set price</p>
+                        </button>
+
+                    </div>
+
+                    <div>
+                        
+                    </div>
+                </div>
+                <div className='center'>
+                    <p>div 2</p>
+                </div>
+                <div className='right'>
+                    <p>div3</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default main
